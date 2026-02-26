7df191c724bf0c125224362dfc63dbaa4d44c3ea
Junyao <junyaokh822@gmail.com>
2026-02-26 16:02:48 -0500

    update Cart schema

---
+++ models/Cart.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, "Quantity cannot be less than 1"],
+    default: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  name: String,
+  imageUrl: String,
+});
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+    totalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Calculate total price before saving
+cartSchema.pre("save", function (next) {
+  this.totalPrice = this.items.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+  next();
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+export default Cart;
