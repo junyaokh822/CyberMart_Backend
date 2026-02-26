d8b82624a75fd2f228f46cdfa98854326571dbdc
Junyao <junyaokh822@gmail.com>
2026-02-26 17:04:39 -0500

    testing route for models

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { logReq, globalErr } from "./middleware/basicMiddlewares.js";
 import dotenv from "dotenv";
 import connectDB from "./db/conn.js";
+import User from "./models/User.js";
+import Product from "./models/Product.js";
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,25 @@ app.use(logReq);
 //routes
 app.get("/", (req, res) => {
   res.json({ message: "Backend is working!" });
+});
+
+// Temporary test route for models
+app.get("/api/test-models", async (req, res) => {
+  try {
+    const models = {
+      user: await User.findOne().select("-password"),
+      product: await Product.findOne(),
+    };
+    res.json({
+      message: "Models are working!",
+      modelsDefined: {
+        user: !!models.user,
+        product: !!models.product,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 //global handling errors
